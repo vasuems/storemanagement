24a2fbe1486205d2aebf4a58fb4fb9330be48fc5
ccwukong <ccwukong@gmail.com>
2018-08-18 14:25:24 +0800

    updated mock data and screenshot

--- src/apis/mocks/responses.js
+++ src/apis/mocks/responses.js
@@ -2,38 +2,50 @@ export const newProducts = [
   {
     productName: 'Test Product 1',
     productURL: 'http://localhost:3000/products/test-product-1',
-    productImage: 'https://image.spreadshirtmedia.com/image-server/v1/products/1003716746/views/1,width=800,height=800,appearanceId=1,backgroundColor=fff,version=1485256808/i-eat-ass-t-shirt-men-s-t-shirt.jpg',
-    productPrice: '$12.90',
+    productImage: 'https://i.ebayimg.com/images/g/JBsAAOSwVLRaXsz4/s-l500.png',
+    productPrice: '$290.00',
   },
   {
     productName: 'Test Product 2',
     productURL: 'http://localhost:3000/products/test-product-2',
-    productImage: 'http://www.toysandgeek.com/wp-content/uploads/2016/04/008-840x560.jpg',
+    productImage: 'https://i.ebayimg.com/images/g/J~cAAOSw5cNYK7I-/s-l1600.jpg',
     productPrice: '$19.90',
   },
   {
     productName: 'Test Product 3',
     productURL: 'http://localhost:3000/products/test-product-3',
-    productImage: 'https://www.uncommongoods.com/images/items/40400/40465_1_1200px.jpg',
-    productPrice: '$22.90',
+    productImage: 'https://i.ebayimg.com/images/g/-I4AAOSwXiZbbINY/s-l1600.jpg',
+    productPrice: '$29.90',
   },
   {
     productName: 'Test Product 4',
     productURL: 'http://localhost:3000/products/test-product-4',
-    productImage: 'http://cdn.shopify.com/s/files/1/0706/7445/products/0e067fea90fbcc6173a542e452a6e24c_grande.png?v=1469603053',
-    productPrice: '$12.90',
+    productImage: 'https://i.ebayimg.com/images/g/kSIAAOSwrnRaYJ3X/s-l1600.png',
+    productPrice: '$999.00',
   },
   {
     productName: 'Test Product 5',
     productURL: 'http://localhost:3000/products/test-product-5',
-    productImage: 'https://s-media-cache-ak0.pinimg.com/736x/e5/b8/fa/e5b8fa101264808f877497808f25f08b.jpg',
-    productPrice: '$12.90',
+    productImage: 'https://i.ebayimg.com/images/g/fBYAAOSwO9RbKfju/s-l1600.jpg',
+    productPrice: '$120.90',
   },
   {
     productName: 'Test Product 6',
     productURL: 'http://localhost:3000/products/test-product-6',
-    productImage: 'http://assets.teenvogue.com/photos/57dfbfe5046b3a2e2a7364bd/master/pass/Kylie-Armytee.jpg',
-    productPrice: '$12.90',
+    productImage: 'https://i.ebayimg.com/images/g/zY8AAOSwCcZaTOf0/s-l1600.jpg',
+    productPrice: '$238.00',
+  },
+  {
+    productName: 'Test Product 7',
+    productURL: 'http://localhost:3000/products/test-product-7',
+    productImage: 'https://i.ebayimg.com/images/g/nXgAAOSwea5bd2bo/s-l1600.jpg',
+    productPrice: '$450.00',
+  },
+  {
+    productName: 'Test Product 8',
+    productURL: 'http://localhost:3000/products/test-product-8',
+    productImage: 'https://i.ebayimg.com/images/g/lEwAAOSw0sZbcgc1/s-l1600.jpg',
+    productPrice: '$1,140.00',
   },
 ];
 
@@ -47,7 +59,7 @@ export const featuredProducts = [
   {
     productName: 'Test Product 2',
     productURL: 'http://localhost:3000/products/test-product-2',
-    productImage: 'http://www.toysandgeek.com/wp-content/uploads/2016/04/008-840x560.jpg',
+    productImage: 'https://i.ebayimg.com/images/g/lNoAAOSwTM5Y524t/s-l1600.jpg',
     productPrice: '$19.90',
   },
   {
@@ -59,8 +71,8 @@ export const featuredProducts = [
   {
     productName: 'Test Product 4',
     productURL: 'http://localhost:3000/products/test-product-4',
-    productImage: 'http://cdn.shopify.com/s/files/1/0706/7445/products/0e067fea90fbcc6173a542e452a6e24c_grande.png?v=1469603053',
-    productPrice: '$12.90',
+    productImage: 'https://admin.chronostore.nyc//media/catalog/product/5/t/5th-ave_19554_chronostore_6.jpg',
+    productPrice: '$11,200.00',
   },
   {
     productName: 'Test Product 5',
@@ -73,6 +85,18 @@ export const featuredProducts = [
     productURL: 'http://localhost:3000/products/test-product-6',
     productImage: 'http://assets.teenvogue.com/photos/57dfbfe5046b3a2e2a7364bd/master/pass/Kylie-Armytee.jpg',
     productPrice: '$12.90',
+  },
+  {
+    productName: 'Test Product 7',
+    productURL: 'http://localhost:3000/products/test-product-7',
+    productImage: 'https://i.ebayimg.com/images/g/lAIAAOSwYvFZMYHW/s-l1600.jpg',
+    productPrice: '$1,429.00',
+  },
+  {
+    productName: 'Test Product 8',
+    productURL: 'http://localhost:3000/products/test-product-8',
+    productImage: 'https://i.ebayimg.com/images/g/MnIAAOSwQTVV8xSM/s-l1600.jpg',
+    productPrice: '$120.00',
   },
 ];
 
